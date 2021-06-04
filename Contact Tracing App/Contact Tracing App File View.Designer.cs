84e01a0c67a9a08cf92c054b104fde9fbd3f3c18
Rashid Cruz <cruzrashidl27@gmail.com>
2021-06-04 14:56:34 +0800

    added back button

--- Contact Tracing App/Contact Tracing App File View.Designer.cs
+++ Contact Tracing App/Contact Tracing App File View.Designer.cs
@@ -28,23 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.fileview_box = new System.Windows.Forms.RichTextBox();
+            this.back_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // fileview_box
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(8, 8);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(440, 375);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
+            this.fileview_box.Location = new System.Drawing.Point(8, 8);
+            this.fileview_box.Name = "fileview_box";
+            this.fileview_box.Size = new System.Drawing.Size(440, 375);
+            this.fileview_box.TabIndex = 0;
+            this.fileview_box.Text = "";
+            // 
+            // back_btn
+            // 
+            this.back_btn.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.back_btn.Location = new System.Drawing.Point(192, 395);
+            this.back_btn.Name = "back_btn";
+            this.back_btn.Size = new System.Drawing.Size(75, 31);
+            this.back_btn.TabIndex = 16;
+            this.back_btn.Text = "Back";
+            this.back_btn.UseVisualStyleBackColor = true;
             // 
             // Contact_Tracing_App_File_View
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(459, 436);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.back_btn);
+            this.Controls.Add(this.fileview_box);
             this.Name = "Contact_Tracing_App_File_View";
             this.Text = "Contact Tracing App";
             this.ResumeLayout(false);
@@ -53,6 +65,7 @@
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox fileview_box;
+        private System.Windows.Forms.Button back_btn;
     }
 }
